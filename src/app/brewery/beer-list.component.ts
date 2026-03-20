b570ee829d1fb97e04997c868a7e48d5ef51bce8
gallomichele <michele.gallo@finconsgroup.com>
2026-03-20 17:05:31 +0100

    feat: exercise1 implementation

--- src/app/brewery/beer-list.component.ts
+++ src/app/brewery/beer-list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BeersData } from './model/beer-data';
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-beer-list',
     templateUrl: './beer-list.component.html',
     styleUrl: './beer-list.component.scss',
+    imports: [DatePipe]
 })
 export class BeerListComponent {
     beers = BeersData;
