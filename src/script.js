278efa8b5ad4ecdf25bda0b6e06b56314813cb6a
Davide Mininni <davide.mininni@finconsgroup.com>
2026-07-13 15:24:30 +0200

    feat: css test

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 /**
  * Exercise notes
  *
- * The three card must look the same.
- * The descriptions of the three methods contain all the information to fill the missing properties / values.
+ * Open the style.css file.
+ *
+ * The client wants the banner to appear floating in the exact center of the screen (both horizontally and vertically),
+ * overlapping everything else. Use the position and transform properties to center it perfectly.
+ *
+ * The close button must be positioned top-right and its style needs to be improved too.
+ *
+ * The list and the button must be styled too, but you CAN'T change the HTML; use advanced selectors.
  */
