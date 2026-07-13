08db5b03b9c8788687e34f422b8ddad79ccfcbc2
Davide Mininni <davide.mininni@finconsgroup.com>
2026-07-13 15:21:45 +0200

    feat: css lesson 2

---
+++ src/script.js
@@ -0,0 +1,9 @@
+/**
+ * Exercise notes
+ *
+ * Open the style.css.
+ *
+ * Your dog ate the CSS selectors and now the page is broken!
+ * Replace each ______ selector with the correct one following the comments.
+ * DO NOT change the index file.
+ */
