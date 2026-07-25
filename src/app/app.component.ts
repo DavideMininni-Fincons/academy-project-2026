b1d8350acda447a2d572e5803c4990e7806a3da8
gallomichele <michele.gallo@finconsgroup.com>
2026-07-25 18:53:09 +0200

    fix: main routes adapted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { BeerListComponent } from './brewery/beer-list/beer-list.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, BeerListComponent],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
