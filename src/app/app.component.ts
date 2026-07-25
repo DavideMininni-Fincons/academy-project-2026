eae86d213e9258f878813cc5af1eb642fc014a27
gallomichele <michele.gallo@finconsgroup.com>
2026-07-25 15:28:23 +0200

    fix: exercise 2 solution fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { BeerListComponent } from './brewery/beer-list/beer-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, BeerListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
