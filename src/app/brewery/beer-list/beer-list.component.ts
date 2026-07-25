7b52d639849fed94b2f8efb2248be9866cea86ab
gallomichele <michele.gallo@finconsgroup.com>
2026-07-25 22:49:10 +0200

    fix: minor changes

--- src/app/brewery/beer-list/beer-list.component.ts
+++ src/app/brewery/beer-list/beer-list.component.ts
@@ -3,7 +3,7 @@ import { BeerCardComponent } from '../beer-card/beer-card.component';
 import { BeerService } from '../service/beer.service';
 import { Beer } from '../model/beer-model';
 import { AsyncPipe } from '@angular/common';
-import { catchError, map, Observable, of, tap } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Component({
@@ -21,45 +21,36 @@ export class BeerListComponent {
   protected beers$: Observable<Beer[]> = this.beerService.getBeers();
 
   ngOnInit(): void {
-    // this.beerService.getBeers().subscribe({
-    //   next: (beers) => {
-    //     this.beers = beers;
-    //   },
-    //   error: (error) => {
-    //     console.error('Error loading beers', error);
-    //   },
-    // });
-  }
-
-  // RxJS operators: tap + map + catchError
-  protected loadBeerById(id: number): void {
-    this.beerService.getBeerById(id)
-      .pipe(
-        tap(() => console.log('Loading beer...')),
-
-        map(beer => ({
-          ...beer,
-          beerName: beer.beerName.toUpperCase()
-        })),
-
-        catchError(error => {
-          console.error('Error loading beer', error);
-          return of(undefined);
-        })
-      )
-      .subscribe();
+    // this.beerService.getBeers()
+    //   .pipe(
+    //     map(beers =>
+    //       beers.map(beer => ({
+    //         ...beer,
+    //         beerName: beer.beerName.trim(),
+    //       }))
+    //     )
+    //   )
+    //   .subscribe({
+    //     next: (beers) => {
+    //       this.beers = beers;
+    //     },
+    //     error: (error) => {
+    //       console.error('Error loading beers', error);
+    //     },
+    //   });
   }
 
   protected deleteBeer(beerId: number): void {
     this.beerService.deleteBeer(beerId)
+      .pipe(
+        tap(() => console.log('Loading beer...')),
+        catchError(error => {
+          console.error('Error deleting beer', error);
+          return of(undefined);
+        })
+      )
       .subscribe({
-        next: () => {
-          console.log('Beer deleted');
-          this.beers$ = this.beerService.getBeers();
-        },
-        error: (error) => {
-          console.error('Delete error', error);
-        }
+        next: () => this.beers$ = this.beerService.getBeers()
       });
   }
 
