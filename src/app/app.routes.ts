a816351d25d5090aa6be8c75e54e0a348d1fe678
gallomichele <michele.gallo@finconsgroup.com>
2026-07-25 18:57:44 +0200

    fix: app routes fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { BeerListComponent } from './brewery/beer-list/beer-list.component';
 import { LESSON_ROUTES } from './lessons/lesson.routes';
+import { BeerListComponent } from './brewery/beer-list/beer-list.component';
 
 export const routes: Routes = [
   ...LESSON_ROUTES,
-  { path: 'beers', component: BeerListComponent },
-  { path: '', redirectTo: 'beers', pathMatch: 'full' },
-  { path: '**', redirectTo: 'beers' }
+  { path: '', component: BeerListComponent }
 ];
